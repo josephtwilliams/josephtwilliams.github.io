89cdb4d05fddf2ece226220424a6d0468ea1f11d
Joseph Williams <64720924+josephtwilliams@users.noreply.github.com>
2022-02-19 16:33:21 +1100

    Change to framework

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+window.sr = ScrollReveal();
+sr.reveal('.navbar', {
+    duration: 1000,
+    origin: 'bottom',
+    useDelay: 'onload',
+    viewFactor: 0.2
+})
+
+sr.reveal('h1', {
+    duration: 2000,
+    delay: 1000,
+    origin: 'bottom',
+    useDelay: 'onload',
+    viewFactor: 0.2
+})
+
+sr.reveal('.dashboard', {
+    duration: 2000,
+    delay: 2000,
+    origin: 'bottom',
+    useDelay: 'onload',
+    viewFactor: 0.2
+})
+
