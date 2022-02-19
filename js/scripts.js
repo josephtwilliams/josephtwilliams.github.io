ce505f23bb0d25a6a667d2d38e82f4605e3c165a
Joseph Williams <64720924+josephtwilliams@users.noreply.github.com>
2022-02-19 17:56:57 +1100

    remove .html

--- js/scripts.js
+++ js/scripts.js
@@ -2,23 +2,27 @@ window.sr = ScrollReveal();
 sr.reveal('.navbar', {
     duration: 1000,
     origin: 'bottom',
-    useDelay: 'onload',
     viewFactor: 0.2
 })
 
-sr.reveal('h1', {
+sr.reveal('h1',  {
     duration: 2000,
     delay: 1000,
     origin: 'bottom',
-    useDelay: 'onload',
     viewFactor: 0.2
 })
+sr.reveal('p',  {
+    duration: 2000,
+    delay: 1000,
+    origin: 'bottom',
+    viewFactor: 0.2
+})
+
 
 sr.reveal('.dashboard', {
     duration: 2000,
     delay: 2000,
     origin: 'bottom',
-    useDelay: 'onload',
     viewFactor: 0.2
 })
 
