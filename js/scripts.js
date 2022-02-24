05a8b76bc51515fb975b819831a3747c5ed6bf8a
Joseph Williams <64720924+josephtwilliams@users.noreply.github.com>
2022-02-24 21:14:16 +1100

    Change scroll reveal

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,9 @@
 window.sr = ScrollReveal();
 sr.reveal('.navbar', {
     duration: 1000,
-    origin: 'bottom',
-    viewFactor: 0.2
+    origin: 'top',
+    viewFactor: 0.2,
+    distance: '50px'
 })
 
 sr.reveal('h1',  {
@@ -13,9 +14,9 @@ sr.reveal('h1',  {
 })
 sr.reveal('p',  {
     duration: 2000,
-    delay: 1000,
+    delay: 2000,
     origin: 'bottom',
-    viewFactor: 0.2
+    viewFactor: 0.1
 })
 
 
@@ -23,6 +24,6 @@ sr.reveal('.dashboard', {
     duration: 2000,
     delay: 2000,
     origin: 'bottom',
-    viewFactor: 0.2
+    viewFactor: 0.1
 })
 
