45c5b7c49b36bcb7151413ca065c1dfef0d091be
Joseph Williams <64720924+josephtwilliams@users.noreply.github.com>
2022-03-18 10:33:53 +1100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,6 @@ window.sr = ScrollReveal();
 sr.reveal('.navbar', {
     duration: 1000,
     origin: 'top',
-    viewFactor: 0.2,
     distance: '50px'
 })
 
@@ -10,20 +9,17 @@ sr.reveal('h1',  {
     duration: 2000,
     delay: 1000,
     origin: 'bottom',
-    viewFactor: 0.2
 })
 sr.reveal('p',  {
     duration: 2000,
-    delay: 2000,
-    origin: 'bottom',
-    viewFactor: 0.1
+    delay: 1000,
+    origin: 'bottom'
 })
 
 
 sr.reveal('.dashboard', {
     duration: 2000,
-    delay: 2000,
-    origin: 'bottom',
-    viewFactor: 0.1
+    delay: 1000,
+    origin: 'bottom'
 })
 
